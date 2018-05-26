7ef304c2058b69bc658a16994fe1a24e9831058e
Andy <andy.duboc@gmail.com>
2018-05-26 18:52:11 -0400

    Decoupled preview logic

---
+++ Assets/TexturePacker/Editor/TexturePreview.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace TexPacker
+{
+    public class TexturePreview
+    {
+        public void Draw(TexturePacker texPacker)
+        {
+            GUILayout.Label("Preview", TexturePackerStyles.Heading);
+
+            GUILayout.BeginVertical(TexturePackerStyles.Section);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            Vector2 previewSize = new Vector2(256, 256);
+            GUILayout.Label("", TexturePackerStyles.MidBox, GUILayout.Width(previewSize.x), GUILayout.Height(previewSize.y));
+            Rect previewRect = GUILayoutUtility.GetLastRect();
+            Rect alphaRect = new Rect(previewRect.x + 5, previewRect.y + 5, previewRect.width - 10, previewRect.height - 10);
+
+            texPacker.ClearProperties();
+
+            Texture2D preview = texPacker.Create();
+            EditorGUI.DrawPreviewTexture(alphaRect, preview);
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndVertical();
+        }
+    }
+}
